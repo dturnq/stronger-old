410785ed0bbda6155618f31476e1cf0d64d15d00
dturnq <dturnq@gmail.com>
2016-11-27 18:07:52 -0800

    Initial Commit

---
+++ Stronger/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Stronger
+//
+//  Created by David Turnquist on 11/27/16.
+//  Copyright © 2016 David Turnquist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
